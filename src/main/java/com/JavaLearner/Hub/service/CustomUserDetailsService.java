2fdefaefacebf959b9c8833f7dab766e02de390f
23bq1a4756-ctrl/ <23bq1a4756@gmail.com>
2026-06-02 10:55:26 +0530

    Custom User Details is a key service updated

--- src/main/java/com/JavaLearner/Hub/service/CustomUserDetailsService.java
+++ src/main/java/com/JavaLearner/Hub/service/CustomUserDetailsService.java
@@ -6,9 +6,7 @@ import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomUserDetailsService
-        implements UserDetailsService {
-
+public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     public CustomUserDetailsService(
